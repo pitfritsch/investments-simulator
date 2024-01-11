cd78d22798e70e0329e515144bd11fa7bc7f126c
Pedro Fritsch <pedrofritsch98@gmail.com>
2024-01-11 09:40:37 -0300

    better naming

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,36 @@
-import { useCallback, useEffect, useState } from 'react'
-import * as VMasker from 'vanilla-masker'
-import './App.css'
+import { useCallback, useEffect, useState } from "react";
+import * as VMasker from "vanilla-masker";
+import "./App.css";
 
 interface IYield {
-  month: number
-  totalValue: number
-  yieldValue: number
-  totalYield: number
-  totalValueWithYield: number
+  month: number;
+  totalValue: number;
+  yieldValue: number;
+  totalYield: number;
+  totalValueWithYield: number;
 }
 
 function returnNumberFromFormattedValue(value: string) {
-  return Number(
-    value.replaceAll('.','').replaceAll(',', '.')
-  )
+  return Number(value.replaceAll(".", "").replaceAll(",", "."));
 }
 
 function App() {
+  const [initialValue, setInitialValue] = useState("");
+  const [monthlyValue, setMonthlyValue] = useState("");
+  const [interest, setInterest] = useState("");
+  const [term, setTerm] = useState("");
 
-  const [initialValue, setInitialValue] = useState('')
-  const [monthlyValue, setMonthlyValue] = useState('')
-  const [fees, setFees] = useState('')
-  const [term, setTerm] = useState('')
-  
-  const [results, setResults] = useState<IYield[]>([])
+  const [results, setResults] = useState<IYield[]>([]);
 
   const recalculate = useCallback(() => {
-    if (!initialValue || !monthlyValue || !fees || !term) return
-    
-    const numberedInitialValue = returnNumberFromFormattedValue(initialValue)
-    const numberedMonthlyValue = returnNumberFromFormattedValue(monthlyValue)
-    const monthlyFee = (returnNumberFromFormattedValue(fees) / 12) / 100
-    const totalMonths = Number(term) * 12
+    if (!initialValue || !monthlyValue || !interest || !term) return;
 
-    const months: IYield[] = []
+    const numberedInitialValue = returnNumberFromFormattedValue(initialValue);
+    const numberedMonthlyValue = returnNumberFromFormattedValue(monthlyValue);
+    const monthlyInterest = returnNumberFromFormattedValue(interest) / 12 / 100;
+    const totalMonths = Number(term) * 12;
+
+    const months: IYield[] = [];
     for (let index = 0; index <= totalMonths; index++) {
       if (index === 0) {
         months.push({
@@ -42,44 +39,43 @@ function App() {
           yieldValue: 0,
           totalYield: 0,
           get totalValueWithYield() {
-            return this.totalValue + this.totalYield
-          }
-        })
-        continue
+            return this.totalValue + this.totalYield;
+          },
+        });
+        continue;
       }
-      const lastMonth = months[index - 1]
-      const monthYield = lastMonth.totalValueWithYield * monthlyFee
+      const lastMonth = months[index - 1];
+      const monthYield = lastMonth.totalValueWithYield * monthlyInterest;
       const newMonth: IYield = {
         month: index,
         totalValue: lastMonth.totalValue + numberedMonthlyValue,
         yieldValue: monthYield,
         totalYield: monthYield + lastMonth.totalYield,
         get totalValueWithYield() {
-          return this.totalValue + this.totalYield
-        }
-      }
+          return this.totalValue + this.totalYield;
+        },
+      };
 
-      months.push(newMonth)
+      months.push(newMonth);
     }
 
-    setResults(months.reverse())
-
-  }, [initialValue, monthlyValue, fees, term])
-
-  useEffect(() => {
-    recalculate()
-  }, [recalculate])
+    setResults(months.reverse());
+  }, [initialValue, monthlyValue, interest, term]);
 
   useEffect(() => {
-    console.log(results)
-  }, [results])
+    recalculate();
+  }, [recalculate]);
+
+  useEffect(() => {
+    console.log(results);
+  }, [results]);
 
   return (
     <div className="container">
       <div>
         <h1>Investments simulator</h1>
-        
-        <div className='flex-column'>
+
+        <div className="flex-column">
           <label htmlFor="initialValue">Initial Value</label>
           <input
             type="text"
@@ -91,7 +87,7 @@ function App() {
         </div>
         <br />
 
-        <div className='flex-column'>
+        <div className="flex-column">
           <label htmlFor="monthlyValue">Monthly contribution</label>
           <input
             type="text"
@@ -103,19 +99,19 @@ function App() {
         </div>
         <br />
 
-        <div className='flex-column'>
-          <label htmlFor="fees">Anual fee</label>
+        <div className="flex-column">
+          <label htmlFor="interest">Annual interest</label>
           <input
             type="text"
-            name="fees"
-            id="fees"
-            value={fees}
-            onChange={(e) => setFees(VMasker.toMoney(e.target.value))}
+            name="interest"
+            id="interest"
+            value={interest}
+            onChange={(e) => setInterest(VMasker.toMoney(e.target.value))}
           />
         </div>
         <br />
 
-        <div className='flex-column'>
+        <div className="flex-column">
           <label htmlFor="term">Term (years)</label>
           <input
             type="number"
@@ -137,7 +133,7 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {results.map(month => 
+          {results.map((month) => (
             <tr key={month.month}>
               <td>{month.month}</td>
               <td>{VMasker.toMoney(month.totalValue.toFixed(2))}</td>
@@ -145,11 +141,11 @@ function App() {
               <td>{VMasker.toMoney(month.totalYield.toFixed(2))}</td>
               <td>{VMasker.toMoney(month.totalValueWithYield.toFixed(2))}</td>
             </tr>
-          )}
+          ))}
         </tbody>
       </table>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
